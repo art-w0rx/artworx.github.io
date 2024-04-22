3bb042864697d4f52209a7d6d54e00a7d3347f2b
Vitalii Bulatnikov <art-worx@outlook.com>
2024-04-23 00:20:03 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,11 +14,23 @@ document.getElementById("contact-btn").addEventListener("click", function() {
 
 //Функция кнопки прокрутки вверх
 function scroll_up() {
-	const scroll_up_btn_left = document.getElementById("scroll_up_btn");
+	const scroll_up_btn = document.getElementById("scroll_up_btn");
 	if (document.documentElement.scrollTop > 0) {
 		window.scroll({ top: 0, left: 0, behavior: 'smooth' });
 	} else {
 		scroll_up_btn.scrollIntoView({ behavior: 'smooth' });
+	}
+}
+
+function scroll_down() {
+	const windowHeight = window.innerHeight;
+	const bodyHeight = document.body.scrollHeight;
+	const scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+
+	if ((windowHeight + scrollPosition) < bodyHeight) {
+		window.scroll({ top: bodyHeight, left: 0, behavior: 'smooth' });
+	} else {
+		window.scroll({ top: document.body.scrollHeight, left: 0, behavior: 'smooth' });
 	}
 }
 
@@ -27,19 +39,28 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 	const scroll_up_btn = document.getElementById("scroll_up_btn");
+	const scroll_down_btn = document.getElementById("scroll_down_btn");
+	const windowHeight = window.innerHeight;
+    const bodyHeight = document.body.scrollHeight;
+	
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 		scroll_up_btn.style.display = "block";
 	} else {
 		scroll_up_btn.style.display = "none";
 	}
+	
+	if ((windowHeight + window.scrollY) < bodyHeight - 20) {
+		scroll_down_btn.style.display = "block";
+	} else {
+		scroll_down_btn.style.display = "none";
+	}
 }
 
-//Фунцкия выезжающего названия текста
-const inf_container = document.getElementById('inf_container');
+// Функция анимации выезжающего справа
 function slideInFromRight(element) {
 	let start = performance.now();
 	function animate(time) {
-		let timeFraction = (time - start) / 400; // Продолжительность анимации в секундах
+		let timeFraction = (time - start) / 600; // Продолжительность анимации в секундах
 		if (timeFraction > 1) timeFraction = 1;
 		let progress = timeFraction;
 		element.style.transform = `translateX(${(1 - progress) * 100}%)`;
@@ -49,14 +70,12 @@ function slideInFromRight(element) {
 	}
 	requestAnimationFrame(animate);
 }
-slideInFromRight(inf_container);
 
-//Фунцкия выезжающего аватара
-const avatar = document.getElementById('avatar');
+// Функция анимации выезжающего слева
 function slideInFromLeft(element) {
 	let start = performance.now();
 	function animate(time) {
-		let timeFraction = (time - start) / 400; // Продолжительность анимации в секундах
+		let timeFraction = (time - start) / 600; // Продолжительность анимации в секундах
 		if (timeFraction > 1) timeFraction = 1;
 		let progress = timeFraction;
 		element.style.transform = `translateX(${(-1 + progress) * 100}%)`;
@@ -66,4 +85,40 @@ function slideInFromLeft(element) {
 	}
 	requestAnimationFrame(animate);
 }
-slideInFromLeft(avatar);
+
+// Функция анимации появления
+function fadeIn(element) {
+	let start = performance.now();
+	function animate(time) {
+		let timeFraction = (time - start) / 800; // Продолжительность анимации в миллисекундах
+		if (timeFraction > 1) timeFraction = 1;
+		let progress = timeFraction;
+		element.style.opacity = progress;
+		if (timeFraction < 1) {
+			requestAnimationFrame(animate);
+		}
+	}
+	requestAnimationFrame(animate);
+}
+
+// Создаем Intersection Observer для анимации выезжающих элементов
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			if (entry.target.id === 'avatar') {
+				slideInFromRight(entry.target);
+			} else if (entry.target.id === 'name') {
+				slideInFromLeft(entry.target);
+			} else if (entry.target.id === 'dealing') {
+				fadeIn(entry.target);
+			} else if (entry.target.id === 'about_me_container' || entry.target.id === 'portfolio_container' || entry.target.id === 'contact_section') {
+				fadeIn(entry.target);
+			}
+		}
+	});
+});
+
+// Наблюдаем за всеми элементами для анимации
+document.querySelectorAll('#name, #avatar, #dealing, #about_me_container, #portfolio_container, #contact_section').forEach(element => {
+	observer.observe(element);
+});
